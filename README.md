73fb1edd74e0ab6256b75d46a83ecf544eb0f8d6
Adrian <Kubokaadrian@outlook.com>
2022-08-29 10:21:39 +0300

    BASICS SETUP
    The nguard class has been set up with the basic guard methods in place.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# NGUARD
+
+A simple library that provides guard clauses for domain modelling.
---
+++ nguard/nguard.py
@@ -0,0 +1,54 @@
+
+class NGuard():
+    """A guard class to ensure the arguments in a constructor or method
+       are valid"""
+
+    def not_null(self, arg1):
+        '''Ensures the input argument is not null'''
+        if arg1 is None:
+            raise Exception("{} should not be null".format(arg1))
+        return self
+
+    def greater_than(self, arg1, arg2):
+        """Ensures the input argument is greater than a given threshold"""
+        if arg1 <= arg2 :
+            raise Exception("{} should be greater than {}".format(arg1, arg2))
+        return self
+
+    def less_than(self, arg1, arg2):
+        """Ensures the input argument is less than a given threshold"""
+        if arg1 >= arg2 :
+            raise Exception("{} should be less than {}".format(arg1, arg2))
+        return self
+
+    def is_int(self, arg):
+        """Ensures the input argument is type int"""
+        self.is_type(arg, int)
+        return self
+
+    def is_string(self, arg):
+        """Ensures the input argument is type string"""
+        self.is_type(arg, str)
+        return self
+
+    def is_float(self, arg):
+        """Ensures the input argument is type float"""
+        self.is_type(arg, float)
+        return self
+
+    def is_list(self, arg):
+        """Ensures the input argument is type list"""
+
+        self.is_type(arg, list)
+        return self
+        
+    def is_dictionary(self, arg):
+        """Ensures the input argument is type dictionary"""
+        self.is_type(arg, dict)
+        return self
+
+    def is_type(self, x, y):
+        if x != type(y):
+            raise Exception("{} is not {} type".format(x, y))
+    
+        
